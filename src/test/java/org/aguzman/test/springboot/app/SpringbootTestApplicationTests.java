a4569d51c05b0674988020825df47c1480a59cf4
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-27 11:14:22 -0300

    Escribiendo tests con assertSame.

--- src/test/java/org/aguzman/test/springboot/app/SpringbootTestApplicationTests.java
+++ src/test/java/org/aguzman/test/springboot/app/SpringbootTestApplicationTests.java
@@ -64,6 +64,9 @@ class SpringbootTestApplicationTests {
 
 		verify(bancoRepository, times(2)).findById(1L);
 		verify(bancoRepository).update(any(Banco.class));
+
+		verify(cuentaRepository, times(6)).findById(anyLong());
+		verify(bancoRepository, never()).findAll();
 	}
 
 	@Test
@@ -98,5 +101,23 @@ class SpringbootTestApplicationTests {
 
 		verify(bancoRepository, times(1)).findById(1L);
 		verify(bancoRepository, never()).update(any(Banco.class));
+
+		verify(cuentaRepository, times(6)).findById(anyLong());
+		verify(bancoRepository, never()).findAll();
+	}
+
+	@Test
+	void contextLoads3() {
+		when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+
+		Cuenta cuenta1 = cuentaService.findById(1L);
+		Cuenta cuenta2 = cuentaService.findById(1L);
+
+		assertSame(cuenta1, cuenta2);
+		assertTrue(cuenta1 == cuenta2);
+		assertEquals("Andrés", cuenta1.getPersona());
+		assertEquals("Andrés", cuenta2.getPersona());
+
+		verify(cuentaRepository, times(2)).findById(1L);
 	}
 }
