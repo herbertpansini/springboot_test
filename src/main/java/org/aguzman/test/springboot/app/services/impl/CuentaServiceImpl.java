e8bae1af6a54bda720164dfe8afffe8e9e47a485
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-27 10:31:44 -0300

    Escribiendo nuestros tests con JUnit y Mockito.

---
+++ src/main/java/org/aguzman/test/springboot/app/services/impl/CuentaServiceImpl.java
@@ -0,0 +1,50 @@
+package org.aguzman.test.springboot.app.services.impl;
+
+import org.aguzman.test.springboot.app.models.Banco;
+import org.aguzman.test.springboot.app.models.Cuenta;
+import org.aguzman.test.springboot.app.repositories.BancoRepository;
+import org.aguzman.test.springboot.app.repositories.CuentaRepository;
+import org.aguzman.test.springboot.app.services.CuentaService;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements CuentaService {
+    private CuentaRepository cuentaRepository;
+    private BancoRepository bancoRepository;
+
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, BancoRepository bancoRepository) {
+        this.cuentaRepository = cuentaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public Cuenta findById(Long id) {
+        return this.cuentaRepository.findById(id);
+    }
+
+    @Override
+    public int revisarTotalTransferencias(Long bancoId) {
+        return this.bancoRepository.findById(bancoId).getTotalTransferencias();
+    }
+
+    @Override
+    public BigDecimal revisarSaldo(Long cuentaId) {
+        return this.findById(cuentaId).getSaldo();
+    }
+
+    @Override
+    public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto, Long bancoId) {
+        Banco banco = this.bancoRepository.findById(bancoId);
+        int totalTransferencias = banco.getTotalTransferencias();
+        banco.setTotalTransferencias(++totalTransferencias);
+        this.bancoRepository.update(banco);
+
+        Cuenta cuentaOrigen = this.findById(numCuentaOrigen);
+        cuentaOrigen.debito(monto);
+        this.cuentaRepository.update(cuentaOrigen);
+
+        Cuenta cuentaDestino = this.findById(numCuentaDestino);
+        cuentaDestino.credito(monto);
+        this.cuentaRepository.update(cuentaDestino);
+    }
+}
--- src/test/java/org/aguzman/test/springboot/app/SpringbootTestApplicationTests.java
+++ src/test/java/org/aguzman/test/springboot/app/SpringbootTestApplicationTests.java
@@ -1,13 +1,50 @@
 package org.aguzman.test.springboot.app;
 
+import org.aguzman.test.springboot.app.repositories.BancoRepository;
+import org.aguzman.test.springboot.app.repositories.CuentaRepository;
+import org.aguzman.test.springboot.app.services.CuentaService;
+import org.aguzman.test.springboot.app.services.impl.CuentaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
 
-	@Test
-	void contextLoads() {
+	CuentaRepository cuentaRepository;
+	BancoRepository bancoRepository;
+	CuentaService cuentaService;
+
+	@BeforeEach
+	void setup() {
+		cuentaRepository = mock(CuentaRepository.class);
+		bancoRepository = mock(BancoRepository.class);
+		cuentaService = new CuentaServiceImpl(cuentaRepository, bancoRepository);
 	}
 
+	@Test
+	void contextLoads() {
+		when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+		when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+		when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+
+		BigDecimal saldoOrigen = cuentaService.revisarSaldo(1L);
+		BigDecimal saldoDestino = cuentaService.revisarSaldo(2L);
+
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		cuentaService.transferir(1L, 2L, new BigDecimal("100"), 1L);
+		saldoOrigen = cuentaService.revisarSaldo(1L);
+		saldoDestino = cuentaService.revisarSaldo(2L);
+
+		assertEquals("900", saldoOrigen.toPlainString());
+		assertEquals("2100", saldoDestino.toPlainString());
+	}
 }
