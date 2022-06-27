03884f590a202b0f564c5fe9cd3fe1aa87f255e4
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-27 16:07:12 -0300

    Escribiendo pruebas de integracion con @DataJpaTest.

--- src/main/java/org/aguzman/test/springboot/app/repositories/CuentaRepository.java
+++ src/main/java/org/aguzman/test/springboot/app/repositories/CuentaRepository.java
@@ -4,6 +4,9 @@ import org.aguzman.test.springboot.app.models.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+    Optional<Cuenta> findByPersona(String persona);
 }
