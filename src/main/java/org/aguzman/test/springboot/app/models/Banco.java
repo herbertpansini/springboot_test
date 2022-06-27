c067ce17d6a5bba8fc45ec7e2dc92f256edf9cfd
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-06-27 12:07:07 -0300

    Configurando el contexto de persistencia JPA y clases entities para test.

--- src/main/java/org/aguzman/test/springboot/app/models/Banco.java
+++ src/main/java/org/aguzman/test/springboot/app/models/Banco.java
@@ -6,13 +6,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bancos")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Banco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    @Column(name = "total_transferencias")
     private int totalTransferencias;
 }
--- src/main/java/org/aguzman/test/springboot/app/models/Cuenta.java
+++ src/main/java/org/aguzman/test/springboot/app/models/Cuenta.java
@@ -7,14 +7,23 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aguzman.test.springboot.app.exceptions.DineroInsuficienteException;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "cuentas")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Cuenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String persona;
     private BigDecimal saldo;
